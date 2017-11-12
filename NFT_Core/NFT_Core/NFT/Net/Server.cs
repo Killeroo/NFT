3d5810d6a70c465e280735ff3c98cd4d61da4d49
Killeroo <matthewcarney64@gmail.com>
2017-11-12 23:50:57 +0000

    Added scan to Server

--- NFT_Core/NFT_Core/NFT/Net/Server.cs
+++ NFT_Core/NFT_Core/NFT/Net/Server.cs
@@ -10,6 +10,7 @@ using System.Net.Sockets;
 
 using NFT.Logger;
 using NFT.Core;
+using NFT.Net;
 
 namespace NFT_Core.NFT.Net
 {
@@ -67,7 +68,71 @@ namespace NFT_Core.NFT.Net
         private async Task HandleConnectionAsync(TcpClient client) { }
         private async Task HandleDisconnectAsync() { }
 
-        private async Task Scan() { }
+        private async Task Scan(string range)
+        {
+            IPEndPoint scanEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Constants.COMMAND_LISTEN_PORT);
+            List<Client> foundClients = new List<Client>();
+            bool rangeFound = false;
+            int hostCount = 0;
+
+            // Split ip address into segments
+            string[] addressSegs = range.Split('.');
+
+            // Check range is specified
+            foreach (var seg in addressSegs)
+                if (seg.Contains("-"))
+                    rangeFound = true;
+
+            // Exit if we havent found a range
+            if (!rangeFound)
+            {
+                Log.Error(new Error(new Exception(), "Scan range not specified"));
+                return;
+            }
+
+            // Holds the ranges for each IP segment
+            int[] segLower = new int[4];
+            int[] segUpper = new int[4];
+
+            // Work out upper and lower ranges for each segment
+            for (int x = 0; x < 4; x++)
+            {
+                string[] ranges = addressSegs[x].Split('-');
+                segLower[x] = Convert.ToInt16(ranges[0]);
+                segUpper[x] = (ranges.Length == 1) ? segLower[x] : Convert.ToInt16(ranges[1]);
+            }
+
+            // Loop through each possible address
+            Log.Info("Scanning for NFT Clients on range [" + range + "]");
+            for (int seg1 = segLower[0]; seg1 <= segUpper[0]; seg1++)
+            {
+                for (int seg2 = segLower[1]; seg2 <= segUpper[1]; seg2++)
+                {
+                    for (int seg3 = segLower[2]; seg3 <= segUpper[2]; seg3++)
+                    {
+                        for (int seg4 = segLower[3]; seg4 <= segUpper[3]; seg4++)
+                        {
+                            // Update with current address to try and connect to
+                            scanEP.Address = new IPAddress(new byte[] { (byte)seg1, (byte)seg2, (byte)seg3, (byte)seg4 });
+                            Client s = new Client(scanEP);
+
+                            // Attempt to connect
+                            //s.Connect();
+                            HandleConnectionAsync(Client);
+
+                            // Add to list of connected Clients
+                            if (s.IsConnected)
+                            {
+                                //ConnectedClients.Add(s);
+                                //hostCount++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Log.Info("Scan complete. " + hostCount + " host(s) found");
+        }
         private async Task Send(Command c) { }
         private async Task Broadcast() { }
 
